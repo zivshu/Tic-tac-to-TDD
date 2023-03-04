f461c5fa8effb399b79e3ee36aa84f6fd6ad7c0a
galbenarushwix <82796762+galbenarushwix@users.noreply.github.com>
2023-03-04 18:32:04 +0200

    step 03 - props drilling current player (#3)

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,14 @@ import App from './App';
 
 test('renders Tic tac toe heading', () => {
   render(<App />);
-  const headingElement = screen.getByRole('heading');
+  const [headingElement] = screen.getAllByRole('heading');
   expect(headingElement).toHaveTextContent(/tic tac toe/i);
+});
+
+test('should render current player', () => {
+  render(<App />);
+  const headingElement = screen.getByText(/current player/i);
+  expect(headingElement).toBeInTheDocument()
 });
 
 test('renders Tic Tac Toe Table', () => {
@@ -17,12 +23,15 @@ test('renders Tic Tac Toe Table', () => {
   expect(cells).toHaveLength(9)
 });
 
-test('cell should render click counter', () => {
-  render(<App />);
-  const [cell] = screen.getAllByRole('cell');
-  expect(cell).toHaveTextContent(0)
-  cell.click()
-  expect(cell).toHaveTextContent(1)
-  cell.click()
-  expect(cell).toHaveTextContent(2)
+test('cell click - should render current player', () => {
+  const [cell1, cell2] = screen.getAllByRole('cell');
+  expect(cell1).toBeEmptyDOMElement()
+  cell1.click()
+  expect(cell1).toHaveTextContent('X')
+  cell1.click()
+  expect(cell1).toHaveTextContent('X') // should stay X
+
+  expect(cell2).toBeEmptyDOMElement()
+  cell2.click()
+  expect(cell2).toHaveTextContent('O')
 });
