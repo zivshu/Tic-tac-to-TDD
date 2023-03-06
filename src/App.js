a1a3e0929c65538922861c3fe2ba25da11b5fc5d
Galbenarush <galar@wix.com>
2023-03-06 21:28:21 +0200

    initial app state

--- src/App.js
+++ src/App.js
@@ -1,82 +1,22 @@
-import { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { isBoardFull, getWinner } from "./utils";
-
-const Cell = ({ val, onCellClicked }) => <td onClick={onCellClicked}>{val}</td>;
-
-
-const Row = ({ cells, onCellClicked, index }) => (
-  <tr>
-    {cells.map((val, cellIndex) => (
-      <Cell
-        key={cellIndex}
-        val={val}
-        onCellClicked={() => onCellClicked(index, cellIndex)}
-      />
-    ))}
-  </tr>
-);
-
-const initBoard = (n) => {
-  const board = [];
-  for (let i = 0; i < n; i++) {
-    board.push([]);
-    for (let j = 0; j < n; j++) {
-      board[i].push(null);
-    }
-  }
-  return board;
-};
-
-const cloneBoard = (board) => board.map((row) => [...row]);
 
 function App() {
-  const [currentPlayer, setCurrentPlayer] = useState("X");
-  const [isGameOver, setIsGameOver] = useState(false);
-  const [winner, setWinner] = useState(false);
-  const toggleCurrentPlayer = () =>
-    currentPlayer === "X" ? setCurrentPlayer("O") : setCurrentPlayer("X");
-
-  const [board, setBoard] = useState(initBoard(3));
-  const onCellClicked = (rowIndex, cellIndex) => {
-    if (board[rowIndex][cellIndex] === null && !winner) {
-      const clonedBoard = cloneBoard(board);
-      clonedBoard[rowIndex][cellIndex] = currentPlayer;
-      setBoard(clonedBoard);
-      const _winner = getWinner(clonedBoard);
-      if (_winner) {
-        setWinner(_winner);
-        setIsGameOver(true);
-      } else if (isBoardFull(clonedBoard)) {
-        setIsGameOver(true);
-      } else {
-        toggleCurrentPlayer();
-      }
-    }
-  };
-
-  const title = winner
-  ? `Winner winner chicken dinner: ${winner}`
-  : isGameOver
-  ? `Sometimes in life - it's a draw..`
-  : `Current player is: ${currentPlayer}`;
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Tic tac toe</h1>
-        <h2>{title}</h2>
-        <table>
-          <tbody>
-          {board.map((cells, i) => (
-              <Row
-                key={i}
-                index={i}
-                cells={cells}
-                onCellClicked={onCellClicked}
-              />
-            ))}
-          </tbody>
-        </table>
+      <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,37 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders Tic tac toe heading', () => {
+
+test('renders learn react link', () => {
   render(<App />);
-  const [headingElement] = screen.getAllByRole('heading');
-  expect(headingElement).toHaveTextContent(/tic tac toe/i);
-});
-
-test('should render current player', () => {
-  render(<App />);
-  const headingElement = screen.getByText(/current player/i);
-  expect(headingElement).toBeInTheDocument()
-});
-
-test('renders Tic Tac Toe Table', () => {
-  render(<App />);
-  const tableElement = screen.getByRole('table');
-  expect(tableElement).toBeInTheDocument()
-  const rows = screen.getAllByRole('row');
-  expect(rows).toHaveLength(3)
-  const cells = screen.getAllByRole('cell');
-  expect(cells).toHaveLength(9)
-});
-
-test('cell click - should render current player', () => {
-  const [cell1, cell2] = screen.getAllByRole('cell');
-  expect(cell1).toBeEmptyDOMElement()
-  cell1.click()
-  expect(cell1).toHaveTextContent('X')
-  cell1.click()
-  expect(cell1).toHaveTextContent('X') // should stay X
-
-  expect(cell2).toBeEmptyDOMElement()
-  cell2.click()
-  expect(cell2).toHaveTextContent('O')
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
